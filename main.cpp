c48c9a2f4be725e85f75d26c423110640cb21ae4
peter nomikos <p.nomikos@meazon.com>
2022-06-17 17:00:21 +0300

    Added a class for the disjoint set union algorithm.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,64 @@
 
 using namespace std;
 
+class disjoinedSet{
+  int* rank;
+  int* parent;
+  int n;
+public:
+  disjoinedSet(int n)
+  {
+    this->n = n;
+  }
+
+  // Finds set of given item x
+  int find(int x)
+  {
+    // Finds the representative of the set
+    // that x is an element of
+    if (parent[x] != x) {
+    // if x is not the parent of itself
+    // Then x is not the representative of
+    // his set,
+    parent[x] = find(parent[x]);
+    // so we recursively call Find on its parent
+    // and move i's node directly under the
+    // representative of this set
+    }
+
+    return parent[x];
+  }
+  // Do union of two sets represented
+   // by x and y.
+   void Union(int x, int y)
+   {
+       // Find current sets of x and y
+       int xset = find(x);
+       int yset = find(y);
+
+       // If they are already in same set
+       if (xset == yset)
+           return;
+
+       // Put smaller ranked item under
+       // bigger ranked item if ranks are
+       // different
+       if (rank[xset] < rank[yset]) {
+           parent[xset] = yset;
+       }
+       else if (rank[xset] > rank[yset]) {
+           parent[yset] = xset;
+       }
+
+       // If ranks are same, then increment
+       // rank.
+       else {
+           parent[yset] = xset;
+           rank[xset] = rank[xset] + 1;
+       }
+   }
+};
+
 void link(Node *child , Node * father)
 {
   child->parent = father;
